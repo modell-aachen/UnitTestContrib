59bfcc3397e05ad5f5c0aff2e6b72b5b0e3fa0a2
Andre Tebart <tebart@modell-aachen.de>
2017-10-13 13:47:51 +0200

    Adds function mocking feature to ModacTestCase.
    Ticket: none
    
    Release-Note: none

--- test/unit/ModacUnitTestCase.pm
+++ test/unit/ModacUnitTestCase.pm
@@ -7,12 +7,41 @@ use Foswiki::Func;
 
 use FoswikiFnTestCase();
 our @ISA = qw(FoswikiFnTestCase);
+our %mockedFunctions;
+
 
 sub new {
-  my $class = shift;
-  my $this  = $class->SUPER::new(@_);
+	my $class = shift;
+  	my $this  = $class->SUPER::new(@_);
 
-  return $this;
+  	return $this;
+}
+
+sub tear_down {
+    my $this = shift;
+
+    $this->_revertAllMocks();
+    $this->SUPER::tear_down();
+    return;
+}
+
+sub mock {
+	my ($this, $functionToMock, $mockFunction) = @_;
+	no strict 'refs';
+	$mockedFunctions{$functionToMock} = *{$functionToMock};
+	undef *{$functionToMock};
+	*{$functionToMock} = $mockFunction;
+	return;
+}
+
+sub _revertAllMocks {
+	while(my ($functionName, $originalFunction) = each %mockedFunctions) {
+		no strict 'refs';
+		undef *{$functionName};
+		*{$functionName} = $originalFunction;
+	}
+	%mockedFunctions = ();
+	return;
 }
 
 1;
